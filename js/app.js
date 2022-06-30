18ece98311e8aba70e6c38ee8b21c3438de46d53
Lisperguer <felipe.lisperguer.h@gmail.com>
2022-06-30 14:35:12 -0400

    All filter updated

--- js/app.js
+++ js/app.js
@@ -40,19 +40,24 @@ year.addEventListener('change',(e) => {
   filtrarAuto();
 })
 puertas.addEventListener('change',(e) => {
-  datosBusqueda.puertas = e.target.value
+  datosBusqueda.puertas = e.target.value;
+  filtrarAuto();
 })
 transmision.addEventListener('change',(e) => {
   datosBusqueda.transmision = e.target.value
+  filtrarAuto();
 })
 minimo.addEventListener('change',(e) => {
   datosBusqueda.minimo = e.target.value
+  filtrarAuto();
 })
 maximo.addEventListener('change',(e) => {
   datosBusqueda.maximo = e.target.value
+  filtrarAuto();
 })
 color.addEventListener('change',(e) => {
   datosBusqueda.color = e.target.value;
+  filtrarAuto();
 })
 
 //Funciones
@@ -90,11 +95,22 @@ function llenarSelect(){
 //funcion que filtra en base a la busqueda
 
 function filtrarAuto(){
-  const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
+  const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarColor);
   console.log(resultado);
 
-  mostrarAutos(resultado);
+  if (resultado.length){
+    mostrarAutos(resultado);
+  } else {
+    noResultado();
+  }
 };
+
+function noResultado(){
+  const noResultado = document.createElement('div');
+  noResultado.classList.add('alerta','error');
+  noResultado.textContent = 'No hay resultado';
+  resultado.appendChild(noResultado);
+}
 
 function filtrarMarca(auto){
   if (datosBusqueda.marca){
@@ -109,3 +125,36 @@ function filtrarYear(auto){
   }
   return auto;
 }
+
+function filtrarMinimo(auto){
+  if (datosBusqueda.minimo){
+    return auto.precio >= datosBusqueda.minimo;
+  }
+  return auto;
+}
+
+function filtrarMaximo(auto){
+  if (datosBusqueda.maximo){
+    return auto.precio <= datosBusqueda.maximo;
+  }
+  return auto;
+}
+function filtrarPuertas(auto){
+  if (datosBusqueda.puertas){
+    return auto.puertas === parseInt(datosBusqueda.puertas);
+  } 
+  return auto;
+}
+function filtrarColor(auto){
+  if (datosBusqueda.color){
+    return auto.color === datosBusqueda.color;
+  } 
+  return auto;
+}
+function filtrarColor(auto){
+  if (datosBusqueda.transmision){
+    return auto.transmision === datosBusqueda.transmision;
+  } 
+  return auto;
+}
+
